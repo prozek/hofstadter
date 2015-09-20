fcde6dcc1f9bf2bbe98f81708c16d175e071b6ac
prozek <piotrrozek91@gmail.com>
2015-09-20 17:44:49 +0200

    first commit

---
+++ square.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+t  = 1
+L  = 20
+NB = 50
+N2 = L*L 
+
+def neig(i):
+    res = [None] * 4
+
+    res[0] = i+1
+    if(res[0]%L == 1):
+        res[0]=i+1-L
+    
+    res[1] = i-1
+    if(res[1]%L == 0):
+        res[1]=i-1+L
+    
+    res[2] = i+L
+    if(res[2] >= N2):
+        res[2]=(i+L-N2)%N2
+
+    res[3] = i-L
+    if(res[3] <= 0): 
+        res[3]=(i-L+N2)
+
+    return res
+
+def perFac(i,j,B):
+    n = neig(i)
+    if i>j:
+        if j==n[0]:
+            return np.exp(  1j * B * 2 * np.pi )
+        elif j==[1]:
+            return np.exp( -1j * B * 2 * np.pi )
+        else:
+            return 1
+    else:
+        if j==n[0]:
+            return np.exp( -1j * B * 2 * np.pi )
+        elif j==[1]:
+            return np.exp( 1j * B * 2 * np.pi )
+        else:
+            return 1
+
+H = [[0 for i in range(N2)] for i in range(N2)] 
+
+
+def tij(x,y,B):
+    return [[ (perFac(x,y,B) if (i==x and j==y) else 0)  for i in range(N2)] for j in range(N2)] 
+
+
+for B in np.linspace(0,1,NB):
+    H = [[0 for i in range(N2)] for i in range(N2)] 
+    for i in range(1,N2+1):
+        for j in range(0,4,1):
+            H = np.add(H, tij(i-1,neig(i)[j]-1,B) )    
+    #print(H)
+    print(np.linalg.eigh(H)[0])
+
