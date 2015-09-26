5edb59bfcb83304101584bc9cb419982b070649c
prozek <piotrrozek91@gmail.com>
2015-09-26 16:43:22 +0200

    honeycomb fix 26092015

--- honeycomb.py
+++ honeycomb.py
@@ -1,67 +1,79 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import csv
 
 t  = 1
-L  = 20
+L  = 3
 NB = 49
-N2 = L*L 
+N2 = 2*L*2*L
+tot = 2*L*2*L
+
+def ypos(x):
+    rest = x % (2*L)
+    return (x-rest)/(2*L)+1
+
+def xpos(x):
+    return x % (2*L)
 
 def neig(i):
-    res = [None] * 4
+    res = [None] * 3
 
     res[0] = i+1
-    if(res[0]%L == 1):
-        res[0]=i+1-L
-    
-    res[1] = i-1
-    if(res[1]%L == 0):
-        res[1]=i-1+L
-    
-    res[2] = i+L
-    if(res[2] >= N2):
-        res[2]=(i+L-N2)%N2
+    if (res[0] % (2*L) == 1):
+        res[0] -= 2*L
 
-    res[3] = i-L
-    if(res[3] <= 0): 
-        res[3]=(i-L+N2)
+    res[1] = i-1
+    if (res[1] % (2*L) == 0):
+        res[1] += 2*L
+
+    res[2] = i
+    if ( ypos(res[2]) == 1 ):
+        if( res[2] % 2 == 0 ):
+            res[2] += 2*L*2*L-2*L
+        elif( res[2] % 2 == 1):
+            res[2] += 2*L
+    elif ( ypos(res[2]) == 2*L):
+        if( res[2] % 2 == 1):
+            res[2] -= 2*L
+        elif(res[2] % 2 == 0):
+            res[2] -= 2*L*2*L-2*L
+    else:
+        if(ypos(res[2]) % 2 == 0):
+            if (res[2] % 2 == 0):
+                res[2] += 2*L
+            elif(res[2] % 2 == 1):
+                res[2] -= 2*L
+        elif(ypos(res[2]) % 2 == 1):
+            if (res[2] % 2 == 1):
+                res[2] += 2*L
+            elif(res[2] % 2 == 0):
+                res[2] -= 2*L
 
     return res
 
-def ypos(x):
-    rest = x % L
-    return (x-rest)/L
-
-
 def perFac(i,j,B):
-    n = neig(i)
-    if i>j:
-        if j==n[0]:
-            return np.exp(  1j * B * 2 * np.pi * (ypos(i)-1) )
-        elif j==n[1]:
-            return np.exp( -1j * B * 2 * np.pi * (ypos(i)-1) )
-        else:
-            return 1
+    n = neig(i)[2]
+    if(xpos(i) % 2 == 1):
+        return np.exp(  1j * B * 2 * np.pi * (xpos(i)-1) )
+    elif(xpos(i) % 2 == 0):
+        return np.exp(  -1j * B * 2 * np.pi * (xpos(i)-1) )
     else:
-        if j==n[0]:
-            return np.exp( -1j * B * 2 * np.pi * (ypos(i)-1) )
-        elif j==n[1]:
-            return np.exp(  1j * B * 2 * np.pi * (ypos(i)-1) )
-        else:
-            return 1
+        return 1
 
 H = [[0 for i in range(N2)] for i in range(N2)] 
 
 def tij(x,y,B):
-    return [[ (perFac(x,y,B) if (i==x and j==y) else 0)  for i in range(N2)] for j in range(N2)] 
+    return [[ (perFac(i,j,B) if (i==x and j==y) else 0)  for i in range(N2)] for j in range(N2)] 
+
+#for i in range(1,tot+1):
+#    print(str(i)+" ; "+str(neig(i)))
 
 with open(str(L)+'x'+str(L)+str(NB) +'.csv', 'wt') as csvfile:
     output = csv.writer(csvfile, delimiter=',',quotechar=' ', quoting=csv.QUOTE_MINIMAL)
-
+    
     for B in np.linspace(0,1,NB):
         H = [[0 for i in range(N2)] for i in range(N2)] 
         for i in range(1,N2+1):
-            for j in range(0,4,1):
-                H = np.add(H, tij(i-1,neig(i)[j]-1,B) )
-
+            for j in range(0,3,1):
+                H = np.add(H, tij(i-1,neig(i)[j]-1,B))
+        
         output.writerow(np.linalg.eigh(H)[0])
